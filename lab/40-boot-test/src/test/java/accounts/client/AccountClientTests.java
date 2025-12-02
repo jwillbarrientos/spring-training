087e361d9be3664946ed9aa534022d4ad8cfff91
barri <barrientosjonah@gmail.com>
2025-12-02 16:44:15 -0300

    thirteenth exercise complete, 40-boot-test

--- lab/40-boot-test/src/test/java/accounts/client/AccountClientTests.java
+++ lab/40-boot-test/src/test/java/accounts/client/AccountClientTests.java
@@ -2,7 +2,11 @@ package accounts.client;
 
 import common.money.Percentage;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import rewards.internal.account.Account;
@@ -14,7 +18,7 @@ import java.util.Random;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-// TODO-00: In this lab, you are going to exercise the following:
+// TOD0-00: In this lab, you are going to exercise the following:
 // - Using @SpringBootTest and webEnvironment for end-to-end testing
 //   (You are going to refactor the test code of previous lab of "38-rest-ws"
 //    to use Spring Boot test framework.)
@@ -24,15 +28,18 @@ import static org.junit.jupiter.api.Assertions.*;
 // - Using MockMvc for Web slice testing
 // - Understanding the difference between @MockBean and @Mock
 
-// TODO-01: Make this class a Spring Boot test class
+// TOD0-01: Make this class a Spring Boot test class
 // - Add @SpringBootTest annotation with WebEnvironment.RANDOM_PORT
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AccountClientTests {
 
-	// TODO-02: Autowire TestRestTemplate bean to a field
+	// TOD0-02: Autowire TestRestTemplate bean to a field
 	// - Name the field as restTemplate
+    @Autowired
+    private TestRestTemplate restTemplate;
 
-	// TODO-03: Update code below to use TestRestTemplate (as opposed to RestTemplate)
+	// TOD0-03: Update code below to use TestRestTemplate (as opposed to RestTemplate)
 	// - Remove RestTemplate from this code
 	// - Remove BASE_URL from this code or change the value of it to ""
 	// - Run the tests and observe that they pass except
@@ -46,7 +53,7 @@ public class AccountClientTests {
 	 */
 	private static final String BASE_URL = "http://localhost:8080";
 
-	private RestTemplate restTemplate = new RestTemplate();
+	//private RestTemplate restTemplate = new RestTemplate();
 	private Random random = new Random();
 
 	@Test
@@ -89,7 +96,7 @@ public class AccountClientTests {
 		assertThat(retrievedAccount.getEntityId()).isNotNull();
 	}
 
-	// TODO-04: Modify the code below so that it handles 404 HTTP response status
+	// TOD0-04: Modify the code below so that it handles 404 HTTP response status
 	//          from the server (instead of handling it as an exception as in the
 	//          case of RestTemplate)
 	// - Remove the "assertThrows" statement (since you are not going to
@@ -108,14 +115,17 @@ public class AccountClientTests {
 
 		restTemplate.delete(newBeneficiaryLocation);
 
-		HttpClientErrorException httpClientErrorException = assertThrows(HttpClientErrorException.class, () -> {
-			System.out.println("You SHOULD get the exception \"No such beneficiary with name 'David'\" in the server.");
-			restTemplate.getForObject(newBeneficiaryLocation, Beneficiary.class);
-		});
-		assertThat(httpClientErrorException.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        ResponseEntity<Beneficiary> response = restTemplate.getForEntity(newBeneficiaryLocation, Beneficiary.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+
+		//HttpClientErrorException httpClientErrorException = assertThrows(HttpClientErrorException.class, () -> {
+		//	System.out.println("You SHOULD get the exception \"No such beneficiary with name 'David'\" in the server.");
+		//	restTemplate.getForObject(newBeneficiaryLocation, Beneficiary.class);
+		//});
+		//assertThat(httpClientErrorException.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
 
-	// TODO-05: Observe a log message in the console indicating
+	// TOD0-05: Observe a log message in the console indicating
 	//          Tomcat started as part of testing
 	// - Search for "Tomcat started on port(s):"
 	// - Note how long it takes for this test to finish - it is
